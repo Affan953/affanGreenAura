5fe4ac3736ccbf0cd6758cb74b40f4e235773d42
Affan672 <affanabdullah1012@gmail.com>
2025-01-23 14:44:21 +0700

    first commit

---
+++ tailwind.config.js
@@ -0,0 +1,13 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: ["./*.html"],
+  theme: {
+    extend: {
+      backgroundImage: {
+        "headers": "url('../img/tanaman.jpg')"
+      }
+    },
+  },
+  plugins: [],
+}
+
